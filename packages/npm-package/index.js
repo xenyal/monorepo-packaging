3134bc0132712c6a2855c4cf65bb6c81a70ca76b
Vincent Li <vinceliii2011@gmail.com>
2021-08-30 16:56:07 -0400

    Testing subsequent commit to a PR that contains prerelease version

--- packages/npm-package/index.js
+++ packages/npm-package/index.js
@@ -1,4 +1,5 @@
 module.exports = function stripSpace(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  console.log("test");
   return string.replace(/\s/g, "");
 };
