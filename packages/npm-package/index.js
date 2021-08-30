20dcfa83efb32cea9db93e1f0338145801e19ea3
Vincent Li <vinceliii2011@gmail.com>
2021-08-30 16:53:47 -0400

    Removing testing commit code change, removed from-package flag

--- packages/npm-package/index.js
+++ packages/npm-package/index.js
@@ -1,5 +1,4 @@
 module.exports = function stripSpace(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  console.log('stripSpace called!');
   return string.replace(/\s/g, "");
 };
