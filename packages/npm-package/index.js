d02ab0588d9f567b51ebe8504bc1060ab9e6e592
Vincent Li <vinceliii2011@gmail.com>
2021-08-30 16:46:12 -0400

    New commit targeting package

--- packages/npm-package/index.js
+++ packages/npm-package/index.js
@@ -1,4 +1,5 @@
 module.exports = function stripSpace(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  console.log('stripSpace called!')
   return string.replace(/\s/g, "");
 };
